3aaa4536693e58c962d367c6ae21b8808b797f9f
ignaszienius <ignaszienius@gmail.com>
2017-08-31 21:15:35 +0300

    troskini padariau veikia

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Troskinio receptas</title>
+
+</head>
+<body>
+<?php
+$troskinys=["jautiena"=>250,"pupeles"=>500,"vanduo"=>500, "pomidorai"=>300,"paprika"=>100];
+$suma = "";
+echo '<table>';
+foreach ($troskinys as $ingridient => $kiekis) {
+	echo  '<tr>' . '<td>' . $ingridient . '</td>' . '<td>' . $kiekis . '</td>'.'<tr>' ; 
+	$suma = $suma + $kiekis;
+}
+
+echo '</table>'	;
+echo "Visu ingridientu suma yra:" . $suma;
+
+
+?>
+</body>
+</html>
