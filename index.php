fabbf5c199fbf066c9831710f11ddbca46da1898
ignaszienius <ignaszienius@gmail.com>
2017-08-31 21:20:45 +0300

    patikrinti ar vandens daugiau nei pupeliu

--- index.php
+++ index.php
@@ -13,9 +13,12 @@ foreach ($troskinys as $ingridient => $kiekis) {
 	echo  '<tr>' . '<td>' . $ingridient . '</td>' . '<td>' . $kiekis . '</td>'.'<tr>' ; 
 	$suma = $suma + $kiekis;
 }
-
 echo '</table>'	;
 echo "Visu ingridientu suma yra:" . $suma;
+
+checkwater($troskinys) {
+	
+}
 
 
 ?>
